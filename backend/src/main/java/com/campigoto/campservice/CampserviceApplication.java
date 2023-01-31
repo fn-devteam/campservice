85999d964fb3bd2aaa8bf90a92528eac5b24064e
FelipeNathan <felipe.campigoto@picpay.com>
2023-01-30 23:25:35 -0300

    Move to monorepo

---
+++ backend/src/main/java/com/campigoto/campservice/CampserviceApplication.java
@@ -0,0 +1,13 @@
+package com.campigoto.campservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampserviceApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/campigoto/campservice/CampserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campigoto.campservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
