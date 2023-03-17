a15f6c37518cf9497c8f385bcfde0723e57e5f4a
FelipeNathan <felipe.campigoto@picpay.com>
2023-03-17 10:50:56 -0300

    Fixing a bug

--- backend/src/main/java/com/campigoto/campservice/config/WebSecurityConfig.java
+++ backend/src/main/java/com/campigoto/campservice/config/WebSecurityConfig.java
@@ -1,64 +1,6 @@
 package com.campigoto.campservice.config;
 
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.Arrays;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig implements WebSecurityConfigurer<WebSecurity> {
-
-	@Override
-	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers(HttpMethod.OPTIONS);
-	}
-
-
-
-	@Override
-	public void init(WebSecurity web) throws Exception {
-		// Não é necessário implementar nada aqui
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-
-		return source;
-	}
-
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
-}
-
-
-
-/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -75,37 +17,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		var cors = new CorsConfiguration().applyPermitDefaultValues();
-		cors.addAllowedMethod("DELETE");
-		cors.addAllowedMethod("PUT");
-		cors.addAllowedMethod("PATCH");
-		cors.addAllowedMethod("OPTIONS");
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var cors = new CorsConfiguration().applyPermitDefaultValues();
+        cors.addAllowedMethod("*");
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", cors);
-		return source;
-	}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
+    }
 
-
-
-	@Configuration
-	@EnableWebSecurity
-	public class SecurityConfiguration {
-
-			@Bean
-			public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-				http
-						.authorizeHttpRequests((authz) -> authz
-								.anyRequest().permitAll()
-						)
-						.httpBasic(withDefaults());
-				return http.build();
-			}
-
-		}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
+                .httpBasic(withDefaults())
+                .cors().and()
+                .csrf().disable()
+                .build();
+    }
 }
-*/
+
 
 
